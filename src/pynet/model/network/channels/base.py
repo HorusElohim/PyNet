ce4cdbcf1b59abcd6f12b0e1ff48cf76a0c8bd66
HorusElohim <HorusElohim@gmail.com>
2022-05-05 00:48:16 +0200

    First Test Channels Success

--- src/pynet/model/network/channels/base.py
+++ src/pynet/model/network/channels/base.py
@@ -1,22 +1,47 @@
 from zmq import Context
-from typing import Any
+from typing import Any, List, Dict
 from ..core import CoreType
 from ..connection import Connection
 from ..transmission import Transmission
 from ..url import BaseUrl
+from ... import Logger
+
+CHA_LOG = Logger('BaseChannel')
 
 
 class BaseChannel:
-    def __init__(self, name: str, core_type: CoreType, context: Context):
+    def __init__(self, name: str, core_type: CoreType, context: Context = Context.instance()):
         self.name = name
         self.core_type = core_type
-        self.connections = []
+        self.connections: Dict[str, Connection] = {}
         self.context = context
+        CHA_LOG.log.debug('done')
 
-    def add(self, url: BaseUrl):
-        self.connections.append(Connection(name=f'{self.name}_{len(self.connections)}', url=url,
-                                           core_type=self.core_type, context=self.context))
+    def add(self, name: str, url: BaseUrl):
+        self.connections.update({
+            name: Connection(name=f'{self.name}.{name}', url=url, core_type=self.core_type, context=self.context)
+        })
+        self.connections[name].open()
+        CHA_LOG.log.debug('done')
 
-    def send(self, data: Any):
-        for con in self.connections:
-            Transmission.send(data)
+    def send(self, data: Any, compression=False) -> List[bool]:
+        res = []
+        for name, con in self.connections.items():
+            res.append(Transmission.send(con, data, compression=compression))
+            CHA_LOG.log.debug(f'{name} recv completed')
+        CHA_LOG.log.debug('done')
+        return res
+
+    def recv(self) -> List[Any]:
+        res = []
+        for name, con in self.connections.items():
+            res.append(Transmission.recv(con))
+            CHA_LOG.log.debug(f'{name} send completed')
+        CHA_LOG.log.debug('done')
+        return res
+
+    def close(self):
+        for name, con in self.connections.items():
+            con.close()
+            CHA_LOG.log.debug(f'{name} close completed')
+        CHA_LOG.log.debug('done')
--- tests/model/network/test_channel.py
+++ tests/model/network/test_channel.py
@@ -1,4 +1,5 @@
 from pynet.model.network.transmission import Packet, Transmission, Connection
+from pynet.model.network.channels.base import BaseChannel
 from pynet.model.network.core import CoreType
 from pynet.model.network.url import Url, BaseUrl
 from dataclassy import dataclass
@@ -14,101 +15,85 @@ def usleep(microsecond):
 DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def test_transmission_packets():
-    c = Connection('Transmission', CoreType.publisher, Url.Local())
-    pkt = Transmission.to_packet(c, DATA)
-    data = Transmission.from_packet(pkt)
-    assert pkt.encoded
-    assert data == DATA
-
-
-def test_transmission_compressed_packets():
-    c = Connection('Transmission', CoreType.publisher, Url.Local())
-    pkt = Transmission.to_packet(c, DATA, compression=True)
-    data = Transmission.from_packet(pkt)
-    assert pkt.encoded
-    assert data == DATA
-
-
 @dataclass(unsafe_hash=True, slots=True)
-class TransmissionTestCase:
+class ChannelTestCase:
     c1_name: str
     c2_name: str
     c1_type: CoreType
     c2_type: CoreType
-    channel: BaseUrl
+    url: BaseUrl
     compression: bool
     data: object
 
 
 # Actions for connection bind
 # publisher, pusher
-def thread_connection_bind(test_case: TransmissionTestCase):
-    c = Connection(test_case.c1_name, test_case.c1_type, test_case.channel)
-    c.open()
+def thread_connection_bind(test_case: ChannelTestCase):
+    c = BaseChannel(test_case.c1_name, test_case.c1_type)
+    c.add(f'Test-{test_case.c1_type}', test_case.url)
     usleep(200)
-    res = Transmission.send(c, test_case.data, compression=test_case.compression)
+    res = c.send(test_case.data, compression=test_case.compression)
     c.close()
-    return res
+    return res[0]
 
 
 #
 # # Actions for connection connect
 # # subscriber, puller
-def thread_connection_connect(test_case: TransmissionTestCase):
-    c = Connection(test_case.c2_name, test_case.c2_type, test_case.channel)
-    c.open()
-    data = Transmission.recv(c)
+def thread_connection_connect(test_case: ChannelTestCase):
+    c = BaseChannel(test_case.c2_name, test_case.c2_type)
+    c.add(f'Test-{test_case.c2_type}', test_case.url)
+    data = c.recv()
     c.close()
-    return data
+    return data[0]
 
 
 # Test Publisher/Subscriber
 # Test Pusher/Puller
 @pytest.mark.parametrize('test_case',
                          [
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Remote(), compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Local(), compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Local(local_type=Url.LocalType().ipc),
-                                                   compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Remote(), compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Local(), compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Local(local_type=Url.LocalType().ipc),
-                                                   compression=False, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Remote(), compression=True, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Local(), compression=True, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
-                                                   c2_name='TestSubscriber', c2_type=CoreType.subscriber,
-                                                   channel=Url.Local(local_type=Url.LocalType().ipc),
-                                                   compression=True, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Remote(), compression=True, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Local(), compression=True, data=DATA, )),
-                             (TransmissionTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
-                                                   c2_name='TestPuller', c2_type=CoreType.puller,
-                                                   channel=Url.Local(local_type=Url.LocalType().ipc),
-                                                   compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Remote(), compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Local(), compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Local(local_type=Url.LocalType().ipc),
+                                              compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Remote(), compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Local(), compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Local(local_type=Url.LocalType().ipc),
+                                              compression=False, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Remote(), compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Local(), compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPublisher', c1_type=CoreType.publisher,
+                                              c2_name='TestSubscriber', c2_type=CoreType.subscriber,
+                                              url=Url.Local(local_type=Url.LocalType().ipc),
+                                              compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Remote(), compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Local(), compression=True, data=DATA, )),
+                             (ChannelTestCase(c1_name='TestPusher', c1_type=CoreType.pusher,
+                                              c2_name='TestPuller', c2_type=CoreType.puller,
+                                              url=Url.Local(local_type=Url.LocalType().ipc),
+                                              compression=True, data=DATA, )),
                          ])
-def test_transmission_pub_sub_pull_push(test_case):
+def test_channel_pub_sub_push_pull(test_case):
     # Parallel Task
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_bind = executor.submit(thread_connection_bind, test_case)
