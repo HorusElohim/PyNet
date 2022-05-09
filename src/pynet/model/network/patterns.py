ce4a903ec1d4c6db98e1f3af61c4aa0b69179a14
HorusElohim <HorusElohim@gmail.com>
2022-05-09 19:42:29 +0200

    Pattern test completed

--- src/pynet/model/network/patterns.py
+++ src/pynet/model/network/patterns.py
@@ -1,3 +1,4 @@
+import abc
 from typing import Any, List, Union
 from .connection import Connection
 from .transmission import Transmission
@@ -6,6 +7,10 @@ from .. import Logger
 
 PATTERN_LOG = Logger('Pattern')
 PATTERN_LOG.log.debug('Module Init')
+
+
+class ConnectionPatternUnsupportedOperation(Exception):
+    pass
 
 
 class PatternBase(Connection):
@@ -20,67 +25,68 @@ class PatternBase(Connection):
                 self.open()
         PATTERN_LOG.log.debug(f'{self} done *')
 
+    def send(self, data: Any, compression: bool = False):
+        PATTERN_LOG.log.debug(f'{self} send')
+        return Transmission.send(self, data, compression=compression)
+
+    def receive(self) -> Any:
+        PATTERN_LOG.log.debug(f'{self} receive')
+        return Transmission.recv(self)
+
 
 class Publisher(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.PUB, urls, auto_open)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
-    def send(self, data: Any, compression: bool = False):
-        Transmission.send(self, data, compression=compression)
+    def receive(self) -> Any:
+        PATTERN_LOG.log.error(f'{self} receive')
+        raise ConnectionPatternUnsupportedOperation
 
 
 class Subscriber(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.SUB, urls, auto_open)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
-    def receive(self) -> Any:
-        return Transmission.recv(self)
+    def send(self, data: Any, compression: bool = False):
+        PATTERN_LOG.log.error(f'{self} send')
+        raise ConnectionPatternUnsupportedOperation
 
 
 class Pusher(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.PUSH, urls, auto_open)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
-    def send(self, data: Any, compression: bool = False):
-        Transmission.send(self, data, compression=compression)
+    def receive(self) -> Any:
+        PATTERN_LOG.log.error(f'{self} receive')
+        raise ConnectionPatternUnsupportedOperation
 
 
 class Puller(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
-        PatternBase.__init__(self, name, connection_type, Connection.PUB, urls, auto_open)
+        PatternBase.__init__(self, name, connection_type, Connection.PULL, urls, auto_open)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
-    def receive(self) -> Any:
-        return Transmission.recv(self)
+    def send(self, data: Any, compression: bool = False):
+        PATTERN_LOG.log.error(f'{self} send')
+        raise ConnectionPatternUnsupportedOperation
 
 
 class Requester(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.REQ, urls, auto_open)
-
-    def send(self, data: Any, compression: bool = False):
-        Transmission.send(self, data, compression=compression)
-
-    def receive(self) -> Any:
-        return Transmission.recv(self)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
 
 class Replier(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.REP, urls, auto_open)
-
-    def send(self, data: Any, compression: bool = False):
-        Transmission.send(self, data, compression=compression)
-
-    def receive(self) -> Any:
-        return Transmission.recv(self)
+        PATTERN_LOG.log.debug(f'{self} done *')
 
 
 class Pair(PatternBase):
     def __init__(self, name: str, connection_type: Connection.Type, urls: Union[Url.BaseUrl, List[Url.BaseUrl], None] = None, auto_open=True):
         PatternBase.__init__(self, name, connection_type, Connection.PAIR, urls, auto_open)
-
-    def send(self, data: Any, compression: bool = False):
-        Transmission.send(self, data, compression=compression)
-
-    def receive(self) -> Any:
-        return Transmission.recv(self)
+        PATTERN_LOG.log.debug(f'{self} done *')
--- tests/model/network/test_5_patterns.py
+++ tests/model/network/test_5_patterns.py
@@ -10,8 +10,8 @@ DATA = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 @dataclass(unsafe_hash=True, slots=True)
 class PatternsTestCase:
-    pattern1: Connection
-    pattern2: Connection
+    pattern1: PatternBase
+    pattern2: PatternBase
     wait1: float
     wait2: float
     url1: BaseUrl
@@ -32,7 +32,7 @@ class PatternSendWorker(Worker):
         self.test_case.pattern1.url = self.test_case.url1
         self.test_case.pattern1.open()
         sleep(self.test_case.wait1)
-        self.result = Transmission.send(self.test_case.pattern1, self.test_case.data, compression=self.test_case.compression)
+        self.result = self.test_case.pattern1.send(self.test_case.data, self.test_case.compression)
         sleep(self.test_case.wait1)
         self.test_case.pattern1.close()
 
@@ -49,7 +49,7 @@ class PatternReceiveWorker(Worker):
         self.test_case.pattern2.url = self.test_case.url2
         self.test_case.pattern2.open()
         sleep(self.test_case.wait2)
-        self.result = Transmission.recv(self.test_case.pattern2)
+        self.result = self.test_case.pattern2.receive()
         sleep(self.test_case.wait2)
         self.test_case.pattern2.close()
 
@@ -90,10 +90,12 @@ def test_transmission_pub_sub_pull_push_server_client(test_case):
     assert worker[1].result == test_case.data
 
 
+#
+
 @dataclass(unsafe_hash=True, slots=True)
 class PatternsAutoTestCase:
-    pattern1: Connection
-    pattern2: Connection
+    pattern1: PatternBase
+    pattern2: PatternBase
     wait1: float
     wait2: float
     data: object
@@ -111,7 +113,7 @@ class PatternSendAutoWorker(Worker):
     def run(self) -> None:
         self.test_case.pattern1.open()
         sleep(self.test_case.wait1)
-        self.result = Transmission.send(self.test_case.pattern1, self.test_case.data, compression=self.test_case.compression)
+        self.result = self.test_case.pattern1.send(self.test_case.data, self.test_case.compression)
         sleep(self.test_case.wait1)
         self.test_case.pattern1.close()
 
@@ -121,30 +123,20 @@ class PatternSendAutoWorker(Worker):
 class PatternReceiveAutoWorker(Worker):
     def __init__(self, test_case):
         Worker.__init__(self)
-        self.test_case: PatternsTestCase = test_case
+        self.test_case: PatternsAutoTestCase = test_case
         self.result = None
 
     def run(self) -> None:
         self.test_case.pattern2.open()
         sleep(self.test_case.wait2)
-        self.result = Transmission.recv(self.test_case.pattern2)
+        self.result = self.test_case.pattern2.receive()
         sleep(self.test_case.wait2)
         self.test_case.pattern2.close()
 
 
-TestCasesAutoPubSubPushPullServerClient = [
+TestCasesAutoPubSubServerClient = [
     # Publisher/Subscriber Server/Client
     # No Compression
-    PatternsAutoTestCase(pattern1=Publisher('PUB', Connection.SERVER, urls=Url.Remote(ip='*'), auto_open=False), wait1=0.4,
-                         pattern2=Subscriber('SUB', Connection.CLIENT, urls=Url.Remote(), auto_open=False), wait2=0.4,
-                         data=DATA, compression=False),
-    PatternsAutoTestCase(pattern1=Publisher('PUB', Connection.SERVER, urls=Url.Local(), auto_open=False), wait1=0.4,
-                         pattern2=Subscriber('SUB', Connection.CLIENT, urls=Url.Local(), auto_open=False), wait2=0.4,
-                         data=DATA, compression=False),
-    PatternsAutoTestCase(pattern1=Publisher('PUB', Connection.SERVER, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait1=0.4,
-                         pattern2=Subscriber('SUB', Connection.CLIENT, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait2=0.4,
-                         data=DATA, compression=False),
-    # With Compression
     PatternsAutoTestCase(pattern1=Publisher('PUB', Connection.SERVER, urls=Url.Remote(ip='*'), auto_open=False), wait1=0.4,
                          pattern2=Subscriber('SUB', Connection.CLIENT, urls=Url.Remote(), auto_open=False), wait2=0.4,
                          data=DATA, compression=False),
@@ -160,8 +152,145 @@ TestCasesAutoPubSubPushPullServerClient = [
 # Test Publisher/Subscriber
 # Test Pusher/Puller
 # Server/Client
-@pytest.mark.parametrize('test_case', TestCasesAutoPubSubPushPullServerClient)
-def test_transmission_auto_open_pub_sub_pull_push_server_client(test_case):
+@pytest.mark.parametrize('test_case', TestCasesAutoPubSubServerClient)
+def test_transmission_auto_open_pub_sub_server_client(test_case):
+    worker = [PatternSendAutoWorker(test_case), PatternReceiveAutoWorker(test_case)]
+    WorkerRunner.run(worker)
+    assert worker[0].result
+    assert worker[1].result == test_case.data
+
+
+TestCasesAutoPushPullServerClient = [
+    # Pusher/Puller Server/Client
+    # No Compression
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Remote(ip='*'), auto_open=False), wait1=0.6,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Remote(), auto_open=False), wait2=0.2,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Local(), auto_open=False), wait1=0.6,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Local(), auto_open=False), wait2=0.2,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait1=0.6,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait2=0.1,
+                         data=DATA, compression=False),
+    # With Compression
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Remote(ip='*', port=28129), auto_open=False), wait1=0.6,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Remote(port=28129), auto_open=False), wait2=0.2,
+                         data=DATA, compression=True),
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait1=0.6,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait2=0.2,
+                         data=DATA, compression=True),
+    PatternsAutoTestCase(pattern1=Pusher('PUSH', Connection.SERVER, urls=Url.Local(path='/tmp/pynet_2', local_type=Url.IPC), auto_open=False),
+                         wait1=0.9,
+                         pattern2=Puller('PULL', Connection.CLIENT, urls=Url.Local(path='/tmp/pynet_2', local_type=Url.IPC), auto_open=False),
+                         wait2=0.2,
+                         data=DATA, compression=True),
+]
+
+
+# Test Publisher/Subscriber
+# Test Pusher/Puller
+# Server/Client
+@pytest.mark.parametrize('test_case', TestCasesAutoPushPullServerClient)
+def test_transmission_auto_open_push_pull_server_client(test_case):
+    worker = [PatternSendAutoWorker(test_case), PatternReceiveAutoWorker(test_case)]
+    WorkerRunner.run(worker)
+    assert worker[0].result
+    assert worker[1].result == test_case.data
+
+
+TestCasesAutoPairServerClient = [
+    # Pair
+    # No Compression
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Remote(ip='*'), auto_open=False), wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Remote(), auto_open=False), wait2=0.3,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait2=0.3,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Local(local_type=Url.IPC), auto_open=False), wait2=0.3,
+                         data=DATA, compression=False),
+]
+
+
+# Test Publisher/Subscriber
+# Test Pusher/Puller
+# Server/Client
+@pytest.mark.parametrize('test_case', TestCasesAutoPairServerClient)
+def test_transmission_auto_open_pair_server_client(test_case):
+    worker = [PatternSendAutoWorker(test_case), PatternReceiveAutoWorker(test_case)]
+    WorkerRunner.run(worker)
+    assert worker[0].result
+    assert worker[1].result == test_case.data
+
+
+# Actions for connection bind
+# replier
+class PatternReplierWorker(Worker):
+    def __init__(self, test_case):
+        Worker.__init__(self)
+        self.test_case: PatternsAutoTestCase = test_case
+        self.result = None
+
+    def run(self) -> None:
+        c = Connection(self.test_case.name1, self.test_case.type1, self.test_case.pattern1)
+        c.url = self.test_case.url1
+        c.open()
+        sleep(self.test_case.wait1)
+        req = Transmission.recv(c)
+        rep = Transmission.send(c, self.test_case.data, compression=self.test_case.compression)
+        c.close()
+        self.result = {
+            'req': req,
+            'rep_result': rep
+        }
+
+
+# # # Actions for connection connect
+# # requester
+class PatternRequesterWorker(Worker):
+    def __init__(self, test_case):
+        Worker.__init__(self)
+        self.test_case: PatternsAutoTestCase = test_case
+        self.result = None
+
+    def run(self) -> None:
+        c = Connection(self.test_case.name2, self.test_case.type2, self.test_case.pattern2)
+        c.url = self.test_case.url2
+        c.open()
+        sleep(self.test_case.wait2)
+        req_res = Transmission.send(c, self.test_case.data, compression=self.test_case.compression)
+        rep = Transmission.recv(c)
+        sleep(self.test_case.wait2)
+        c.close()
+        self.result = {
+            'req_result': req_res,
+            'rep': rep
+        }
+
+
+TestCasesAutoPairReqRep = [
+    # Pair
+    # No Compression
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Remote(ip='*'), auto_open=False), wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Remote(), auto_open=False), wait2=0.3,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Local(path='/tmp/pynet_1'), auto_open=False), wait2=0.3,
+                         data=DATA, compression=False),
+    PatternsAutoTestCase(pattern1=Pair('PAIR1', Connection.SERVER, urls=Url.Local(path='/tmp/pynet_2', local_type=Url.IPC), auto_open=False),
+                         wait1=0.4,
+                         pattern2=Pair('PAIR2', Connection.CLIENT, urls=Url.Local(path='/tmp/pynet_2', local_type=Url.IPC), auto_open=False),
+                         wait2=0.3,
+                         data=DATA, compression=False),
+]
+
+
+# Test Publisher/Subscriber
+# Test Pusher/Puller
+# Server/Client
+@pytest.mark.parametrize('test_case', TestCasesAutoPairReqRep)
+def test_transmission_auto_open_pair_server_client(test_case):
     worker = [PatternSendAutoWorker(test_case), PatternReceiveAutoWorker(test_case)]
     WorkerRunner.run(worker)
     assert worker[0].result
