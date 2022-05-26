15c335097f9eb3f7e1d79f281dd5f052f86f3c73
HorusElohim <HorusElohim@gmail.com>
2022-05-26 17:02:22 +0200

    Adding delta time util

--- src/pynet/model/common/__init__.py
+++ src/pynet/model/common/__init__.py
@@ -1,6 +1,6 @@
 from .singleton import Singleton
 from .ddict import DDict
-from .time import now, now_lite, today
+from .time import now, now_lite, today, delta_millisecond
 from .logger import Logger, LoggerLevel, LoggerCannotWorkIfBothConsoleAndFileAreDisabled
 from .size import Size
 from .profiler import profile
