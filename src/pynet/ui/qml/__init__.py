a7e6bec1d8947b0ea57b61e045a006dfa095dff2
HorusElohim <horuselohim@gmail.com>
2022-09-18 14:52:25 +0200

    Preparing node manager creation

--- src/pynet/ui/qml/__init__.py
+++ src/pynet/ui/qml/__init__.py
@@ -4,3 +4,29 @@ from PySide6.QtCore import QUrl
 
 def url(qml_file: [str]):
     return QUrl.fromLocalFile(pkg_resources.resource_filename('pynet', qml_file))
+
+
+from PySide6.QtCore import QtInfoMsg, QtWarningMsg, QtCriticalMsg, QtFatalMsg
+from PySide6 import QtCore
+from .. import LOG
+
+QML_MSG_STATUS = {
+    QtInfoMsg: 'INFO',
+    QtWarningMsg: 'WARNING',
+    QtCriticalMsg: 'CRITICAL',
+    QtFatalMsg: 'FATAL'
+}
+
+
+def qt_message_handler(mode, context, message):
+    status = "DEBUG"
+
+    if mode in QML_MSG_STATUS:
+        status = QML_MSG_STATUS[mode]
+
+    msg = f'{status}: {message}, {context.file}, {context.line}'
+    LOG.log.debug(msg)
+
+
+QtCore.qInstallMessageHandler(qt_message_handler)
+LOG.log.debug("qInstallMessageHandler activated")
